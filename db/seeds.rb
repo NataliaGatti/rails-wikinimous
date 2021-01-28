79360c2ff2e4b8f284280bbc1d22c4c61dba2571
Natalia Gatti <gattinati.ng@gmail.com>
2021-01-27 22:43:05 -0300

    Rails Wikinimous. Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating 10 articles"
+
+10.times do
+    article = Article.new(
+        title: Faker::Movie.title,
+        content: Faker::Movie.quote
+    )
+    article.save!
+end
+
+puts "10 post created"
